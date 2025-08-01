df7e7559e73b80108adcdd7a76e3e309d812ecf7
Dhanesh <imtony3579@gmail.com>
2025-08-01 23:20:48 +0530

    starting the course

--- rosalind-problem/stronghold/rabbit_recurrence.py
+++
@@ -1,8 +0,0 @@
-def rabbit(month:int, offsprint:int)->int:
-    parent, child = 1,1
-    for i in range(month-1):
-        child, parent = parent, parent + (child*offsprint)
-
-    return child
-
-print(rabbit(34,2))
